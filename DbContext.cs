d6491f63d7004e394a993d0b594855d948d4cfb4
rbryan95 <berd1495@gmail.com>
2023-12-05 14:10:10 -0600

    Added blog

--- DbContext.cs
+++ DbContext.cs
@@ -20,6 +20,6 @@ public class BloggingContext : DbContext
         // *******************************************************************
         // ## last 2 digits of CRN
         // XXX your initials
-        optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=BlogsConsole_22_BRY;User ID=BRY;Password=000576913");
+        optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=BlogsConsole_22_BRY;User ID=brosado1;Password=000576913");
     }
 }
